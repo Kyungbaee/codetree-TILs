4d356b53142d54555544dd10c927e7d52d311cb2
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-09-29 16:05:06 +0900

    Title: 정육면체 한번 더 굴리기, Time: 154ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240929/정육면체 한번 더 굴리기/cube-rounding-again.py
@@ -0,0 +1,61 @@
+from collections import deque
+
+def fill_score(tx, ty, n):
+    visited = score_map.copy()
+    direction = [[0,1], [1,0], [0,-1], [-1,0]]
+    d, cnt, root, visited[tx][ty] = deque([[tx,ty]]), 1, [(tx, ty)], 1
+
+    while d:
+        x, y = d.popleft()
+
+        for mx, my in direction:
+            dx, dy = x+mx, y+my
+
+            if 0<=dx<n and 0<=dy<n and not visited[dx][dy] and dice_map[dx][dy] == dice_map[x][y]:
+                cnt += 1
+                d.append([dx,dy])
+                root.append((dx, dy))
+                visited[dx][dy] = 1
+    
+    for dx, dy in root: score_map[dx][dy] = cnt*dice_map[tx][ty]
+
+def solution(n, m):
+    direction = [[0,1], [1,0], [0,-1], [-1,0]]
+    x, y, d = 0,1,0
+    cnt = score_map[x][y]
+    up, down, right = 4,2,1
+
+    for _ in range(m-1):
+        if (7-up) > dice_map[x][y]:
+            d = (d+1)%4
+        if (7-up) < dice_map[x][y]:
+            d = (d+3)%4
+
+        dx, dy = x+direction[d][0], y+direction[d][1]
+        if 0<=dx<n and 0<=dy<n:
+            cnt += score_map[dx][dy]
+            x, y = dx, dy
+            d_up, d_down, d_right = up, down, right
+
+            if d == 0: d_up, d_right = (7-right), up
+            elif d == 1: d_up, d_down = (7-down), up
+            elif d == 2: d_up, d_right = right, (7-up)
+            elif d == 3: d_up, d_down = down, (7-up) 
+            
+            up, down, right = d_up, d_down, d_right
+        else:
+            d = (d+2)%4
+            x, y = x+direction[d][0], y+direction[d][1]
+            cnt += score_map[x][y]
+
+    return cnt
+
+n, m = map(int, input().split())
+dice_map = [list(map(int, input().split())) for _ in range(n)]
+score_map = [[0]*n for _ in range(n)]
+
+for tx in range(n):
+    for ty in range(n):
+        if not score_map[tx][ty]: fill_score(tx,ty,n)
+
+print(solution(n, m))
