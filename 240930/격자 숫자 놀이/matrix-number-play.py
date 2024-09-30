b2628e36dd96bf3c9cab43fbdce2e9f3d933141a
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-09-30 20:42:07 +0900

    Title: 격자 숫자 놀이, Time: 235ms, Memory: 27MB, Status: Wrong Answer - CodeTree

---
+++ 240930/격자 숫자 놀이/matrix-number-play.py
@@ -0,0 +1,64 @@
+from collections import defaultdict
+
+def solution(h,w):
+    """
+    행의 개수가 열의 개수보다 크거나 같은 경우
+    """
+    if h >= w:
+        max_len = 0
+        for i in range(h):
+            d = defaultdict(int)
+            for j in range(1, int(max(n_map[i]))+1):
+                cnt = n_map[i].count(str(j))
+                if cnt: d[j] += cnt
+            d = sorted(d.items(), key=lambda x: (x[1], x[0]))
+            d = ''.join(list(map(lambda x: (str(x[0])+str(x[1])), d)))
+            n_map[i] = d
+            max_len = max(max_len, len(d))
+
+        for i in range(h):
+            n_map[i] = n_map[i].ljust(max_len, '0')
+    """
+    열의 개수가 행의 개수보다 크거나 같은 경우
+    """
+    if h < w:
+        d_list, max_len = [], 0
+        for i in range(w):
+            d = defaultdict(int)
+            for j in range(h):
+                if n_map[j][i] != "0": d[n_map[j][i]] += 1
+            
+            d = sorted(d.items(), key=lambda x: (x[1], x[0]))
+            d = ''.join(list(map(lambda x: (str(x[0])+str(x[1])), d)))
+            d_list.append(d)
+            max_len = max(max_len, len(d))
+            max_len = min(100, max_len)
+
+        for i in range(len(d_list)):
+            d = d_list[i]
+            for j in range(max_len):
+                if j<len(d): 
+                    n_map[j] = n_map[j][:i] + d[j] + n_map[j][i+1:]
+                else:
+                    n_map[j] = n_map[j][:i] + "0" + n_map[j][i+1:]
+
+
+r,c,k = map(int, input().split())
+n_map = [ "" for _ in range(101)]
+h, w, chk = 3, 3, 1
+for i in range(3): n_map[i]=input().replace(" ","") 
+
+for idx in range(0,100):
+    if r<=h and c<=w and int(n_map[r-1][c-1])==k:
+        print(idx)
+        chk = 0
+        break
+
+    solution(h,w)
+    
+    for i in range(100):
+        if n_map[i] == "": break
+        h = i+1
+        if len(n_map[i]): w = max(w, len(n_map[i]))
+
+if chk: print(-1)
