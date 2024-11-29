3ddffa9cc1d29a5a79c403be2498e7a99928bf9c
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-11-29 14:09:03 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241129/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+
+    if(a>b) cout << 1 << '\n';
+    else cout << 0 << '\n';
+
+    if(a>c) cout << 1 << '\n';
+    else cout << 0 << '\n';
+
+    if(a>d) cout << 1 << '\n';
+    else cout << 0 << '\n';
+
+    if(a>e) cout << 1 << '\n';
+    else cout << 0 << '\n';
+
+    return 0;
+}
