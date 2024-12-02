3aa95eca44a09270b43d7e138b14eb24279453ca
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-12-02 12:26:25 +0900

    Title: 두 개의 직각삼각형, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241202/두 개의 직각삼각형/two-right-triangle.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            if(j>=n-i) cout << " ";
+            else cout << "*";
+        }
+        for(int j=0; j<n; j++)
+        {
+            if(j<i) cout << " ";
+            else cout << "*";
+        }
+        cout << "\n";
+    }
+
+
+    return 0;
+}
