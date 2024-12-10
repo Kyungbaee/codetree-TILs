8d46a13c3ffb18bd2fcb72268ee0cf3296c717f1
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-12-10 22:34:15 +0900

    Title: 격자 위의 편안한 상태, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241210/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int adj[100][100];
+int dx[4] = {-1,0,1,0}, dy[4] = {0,1,0,-1};
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, k;
+    cin >> n >> k;
+
+    while(k--)
+    {
+        int r, c, cnt=0;
+        cin >> r >> c;
+
+        adj[r-1][c-1] = 1;
+
+        int x=r-1, y=c-1;
+        for(int i=0; i<4; i++)
+        {
+            int nx = x+dx[i], ny = y+dy[i];
+            if(0<=nx && nx<n && 0<=ny && ny<n && adj[nx][ny]==1)
+                cnt++;
+        }
+
+        if(cnt>=3) cout << 1;
+        else cout << 0;
+        cout << "\n";
+    }
+
+    return 0;
+}
